3234b2bfb757159cb211390d6863a879c3b5c794
unknown <thunderfooot2255@gmail.com>
2025-08-23 10:15:13 +0530

    bug fixed

--- workers/videoScraper.ts
+++ workers/videoScraper.ts
@@ -3,6 +3,7 @@ import { config } from 'dotenv'
 import fetchVideos from './helpers/fetchVideos'
 import filterOutVideos from './helpers/filterOutVideos'
 import manageUpload from './helpers/manageUpload'
+import axios from 'axios'
 
 config({ path: '../.env' })
 const db = new Database()
@@ -17,12 +18,13 @@ async function uploadManager() {
         console.log(videos.length, ' videos fetched')
 
         console.log('filtering videos')
-        const filteredVideos = await filterOutVideos(videos)
+        const filteredVideos: any = await filterOutVideos(videos)
 
         console.log('uploading videos')
         manageUpload(filteredVideos)
     } catch (error) {
         console.log('system error:', error)
+        await axios.get(`http://xdroid.net/api/message?k=k-7c2c2c6b4e68&t=error+on+btechh&c=upload+error,retrying...`)
         return await uploadManager()
     }
 }
